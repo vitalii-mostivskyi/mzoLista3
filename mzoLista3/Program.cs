5201385d34cfb9971155b30d0fd0183103f185e0
Vitalii Mostivskyi <vitaliymostivskiy@gmail.com>
2015-11-29 21:09:57 +0100

    Initial commit

---
+++ mzoLista3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Configuration;
+
+namespace mzoLista3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvFilePath = ConfigurationManager.AppSettings["csvFilePath"];
+
+            GetStudents(csvFilePath)
+               .ForEach(s => s.Print());
+        }
+
+        private static List<Student> GetStudents(string path)
+        {
+            return File.ReadAllLines(path).Select(l =>
+            {
+                var parts = l.Split("><".ToCharArray());
+
+                return new Student
+                {
+                    Name = parts[1],
+                    Surname = parts[3],
+                };
+            }).ToList();
+        }
+    }
+}

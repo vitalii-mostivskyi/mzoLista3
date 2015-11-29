f8e1b8b3cbe6fb57000bb674c151538777df0b74
Vitalii Mostivskyi <vitaliymostivskiy@gmail.com>
2015-11-29 22:40:45 +0100

    Revert "Return to csv"
    This reverts commit 66768b67fb40c12eea118cb7b989ac48d8c4b830.

---
+++ mzoLista3/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mzoLista3
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("{0} {1}", Name, Surname);
+        }
+    }
+}
